c78e3109f0c205c536e093c8fc93667341879c3a
Kaden Biel <kaden.secondday@gmail.com>
2020-12-18 17:32:17 -0500

    Added Handler to main

--- PyZork/__main__.py
+++ PyZork/__main__.py
@@ -1,27 +1,30 @@
-
 from Library import Items as it
 from Library import Rooms as ro
 from Library import Player as pl
 from Library import NPC as np
+from Library import * as lib
 
 try:
+    """
+    Try/except block to catch users running an outdated Python version
+    """
     startScreen = input("PyZork v1.3\nHit enter to start\n")
 except:
-    print("Your version of python is not compatible with this program, please use Python 3 or up")
+    print("Your version of python is not compatible with this program, please use the lastest Python 3 version")
     end = raw_input("Hit enter to quit")
     quit()
 
-class Game:
+class Handler:
 
     def __init__(self):    
         """
-        Creates a Game object and initializes the game
+        Creates a Handler object to talk to the Library
         """
 
     def process_cmd(self, c):
         """
         process given command
-        params is a list of arguments
+        Turns command into list of arguments and sends to the library
         """
 
 if __name__ == '__main__':
